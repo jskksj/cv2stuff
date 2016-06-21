3daf26e72424120548cada2726dad9bebf551b5a
jskksj <github@simplexapparati.com>
2016-06-21 09:57:59 -0600

    check for either or both folder and token missing

--- cv2stuff/tests/test_configuration_handler.py
+++ cv2stuff/tests/test_configuration_handler.py
@@ -4,6 +4,21 @@ import cv2stuff.configuration_handler as config
 
 
 def test_initialization():
-    with pytest.raises(ValueError) as need_names:
+    with pytest.raises(ValueError):
         cfg = config.ConfigurationHandler('', '')
-        print(need_names.value, cfg)
+        # get rid of PEP8 error for unused cfg
+        assert(cfg)
+
+
+def test_initialization_folder():
+    with pytest.raises(ValueError):
+        cfg = config.ConfigurationHandler('folder', '')
+        # get rid of PEP8 error for unused cfg
+        assert(cfg)
+
+
+def test_initialization_token():
+    with pytest.raises(ValueError):
+        cfg = config.ConfigurationHandler('', 'token')
+        # get rid of PEP8 error for unused cfg
+        assert(cfg)
